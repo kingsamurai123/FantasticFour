8e5f92f2cd184dedb86587c3c65d7e5f07e2ee9c
RRRohit7 <rohit7rrr@gmail.com>
2020-05-01 23:25:52 +0530

    Updates

---
+++ precache-manifest.1a585fa9d71a2998be4b285f652007c4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad961d7a0179e68b6d80fd56d6e6f6ac",
+    "url": "/FantasticFour/index.html"
+  },
+  {
+    "revision": "d0019082c0ee140a1d4e",
+    "url": "/FantasticFour/static/css/main.85433e50.chunk.css"
+  },
+  {
+    "revision": "8b225712ec4959377134",
+    "url": "/FantasticFour/static/js/2.07c619ae.chunk.js"
+  },
+  {
+    "revision": "f7fe43cf5d12b7c6064f2a7e415de116",
+    "url": "/FantasticFour/static/js/2.07c619ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0019082c0ee140a1d4e",
+    "url": "/FantasticFour/static/js/main.fb88709f.chunk.js"
+  },
+  {
+    "revision": "e39436070603749dbbe4",
+    "url": "/FantasticFour/static/js/runtime-main.c6a9bf8f.js"
+  },
+  {
+    "revision": "95bf25408de79ad9b43eb7cdb2793665",
+    "url": "/FantasticFour/static/media/Fear.95bf2540.mp3"
+  },
+  {
+    "revision": "2017e866779a58bbc0bc59de253f84ef",
+    "url": "/FantasticFour/static/media/SeeYouAgain.2017e866.mp3"
+  },
+  {
+    "revision": "3758f0cf8f3d8b4c183cf99057623c28",
+    "url": "/FantasticFour/static/media/ThousandYears.3758f0cf.mp3"
+  },
+  {
+    "revision": "60476842dd4d83b43559e0053fa8ac21",
+    "url": "/FantasticFour/static/media/WickedOnes.60476842.mp3"
+  },
+  {
+    "revision": "1d46ca55b953d4adcab38432e3833c9c",
+    "url": "/FantasticFour/static/media/backgroundforsure.1d46ca55.jpg"
+  },
+  {
+    "revision": "c4095a8df0ca88a64b3cce48040f2c85",
+    "url": "/FantasticFour/static/media/fearthumbnail.c4095a8d.jpg"
+  },
+  {
+    "revision": "a7b79478e724b19ff453c942cb7baa59",
+    "url": "/FantasticFour/static/media/invincible.a7b79478.jpg"
+  },
+  {
+    "revision": "8b474da3600c96ef20e806ef6e75caf4",
+    "url": "/FantasticFour/static/media/seeyouagainthumbnail.8b474da3.jpg"
+  },
+  {
+    "revision": "b5ab09228f4556a7d5831a4a985720eb",
+    "url": "/FantasticFour/static/media/thousandyearsthumbnail.b5ab0922.jpg"
+  },
+  {
+    "revision": "17842b803d286976ff17c74f1f3411d2",
+    "url": "/FantasticFour/static/media/wickedonesthumbnail.17842b80.jpg"
+  }
+]);
